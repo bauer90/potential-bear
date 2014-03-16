bc652e16a972d243279cdaf43b45351b963fce53
bauer90 <erhan.hu@gmail.com>
2014-03-17 01:27:14 +0800

    init a command-line obj-c proj

---
+++ mar17/mar17/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mar17
+//
+//  Created by Erhan Hu on 3/17/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
