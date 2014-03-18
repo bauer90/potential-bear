c814d9f9c3d950c2d10c95b62930f36134aabecf
bauer90 <us.huerhan@gmail.com>
2014-03-18 23:59:36 +0800

    init iOS proj

---
+++ Matchismo/Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Erhan Hu on 3/18/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Matchismo/Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Erhan Hu on 3/18/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
