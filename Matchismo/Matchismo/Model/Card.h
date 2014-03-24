3219e7588956d4e2bbf131c839f0afcd44ec6eeb
bauer90 <us.huerhan@gmail.com>
2014-03-25 03:44:02 +0800

    started hw1

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Erhan Hu on 3/25/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Erhan Hu on 3/25/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
