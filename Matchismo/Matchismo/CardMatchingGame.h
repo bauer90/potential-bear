c3af087f4b13076a3a96a03e6f91a1a36e869bdf
bauer90 <us.huerhan@gmail.com>
2014-04-01 04:45:00 +0800

    CardMatchingGame halfway

---
+++ Matchismo/Matchismo/CardMatchingGame.h
@@ -0,0 +1,25 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by ERHAN HU on 4/1/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PlayingCard.h"
+#import "PlayingCardDeck.h"
+
+@interface CardMatchingGame : NSObject
+
+// desinated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                       usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
---
+++ Matchismo/Matchismo/CardMatchingGame.m
@@ -0,0 +1,71 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by ERHAN HU on 4/1/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+@property (nonatomic, readwrite) NSInteger score;
+@property (nonatomic, strong) NSMutableArray *cards; // of Card
+@end
+
+@implementation CardMatchingGame
+
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+- (NSMutableArray *)cards
+{
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck
+{
+    self = [super init]; //calling NSObject's initializer
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *card = [deck drawRandomCard];
+            if (card) {
+                [self.cards addObject:card];
+            } else {
+                self = nil;
+                break;
+            }
+        }
+    }
+    return self;
+}
+
+- (void)chooseCardAtIndex:(NSUInteger)index
+{
+    Card *card = [self cardAtIndex:index];
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    self.score += matchScore * MATCH_BONUS;
+                } else {
+                    self.score -= MISMATCH_PENALTY;
+                    otherCard.chosen = NO;
+                }
+            }
+            
+            card.chosen = YES;
+        }
+    }
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index
+{
+    return (index <= [self.cards count]) ? self.cards[index] : nil;
+}
+
+@end
