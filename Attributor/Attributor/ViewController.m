7d022ea3c361d5fa871f6778b92e43f31685a615
bauer90 <us.huerhan@gmail.com>
2014-04-11 18:50:02 +0800

    finished lecture 5

--- Attributor/Attributor/ViewController.m
+++ Attributor/Attributor/ViewController.m
@@ -37,7 +37,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName:@3,
+                           NSStrokeColorAttributeName:self.outlineButton.tintColor}
+                   range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle: title
+                                  forState: UIControlStateNormal];
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+-(void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+-(void) preferredFontsChanged: (NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+-(void) usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
 }
 
 - (void)didReceiveMemoryWarning
