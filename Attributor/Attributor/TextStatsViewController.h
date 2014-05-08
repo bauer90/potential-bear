355a0302ee58a7fb4ffdc126426f50785168b0fb
bauer90 <us.huerhan@gmail.com>
2014-05-09 04:17:24 +0800

    half way lecture 6

---
+++ Attributor/Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Erhan Hu on 5/9/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface TextStatsViewController : ViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
---
+++ Attributor/Attributor/TextStatsViewController.m
@@ -0,0 +1,58 @@
+//
+//  TextStatsViewController.m
+//  Attributor
+//
+//  Created by Erhan Hu on 5/9/14.
+//  Copyright (c) 2014 Erhan Hu. All rights reserved.
+//
+
+#import "TextStatsViewController.h"
+
+@interface TextStatsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *colorfulCharactersLabel;
+@property (weak, nonatomic) IBOutlet UILabel *outlinedCharactersLabel;
+@end
+
+@implementation TextStatsViewController
+
+- (void)setTextToAnalyze:(NSAttributedString *)textToAnalyze
+{
+    _textToAnalyze = textToAnalyze;
+    if (self.view.window) [self updateUI]; // updateUI only when this screen is on.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self updateUI];
+}
+
+- (NSAttributedString *)charactersWithAttribute:(NSString *)attributeName
+{
+    NSMutableAttributedString *chars = [[NSMutableAttributedString alloc] init];
+    int index = 0;
+    while (index < [self.textToAnalyze length]) {
+        NSRange range;
+        id value = [self.textToAnalyze attribute:attributeName atIndex:index effectiveRange:&range];
+        if (value) {
+            [chars appendAttributedString:[self.textToAnalyze attributedSubstringFromRange:range]];
+            index = range.location + range.length;
+        } else {
+            index++;
+        }
+    }
+    return chars;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)updateUI
+{
+    self.colorfulCharactersLabel.text = [NSString stringWithFormat:@"%d colorful character(s)", [[self charactersWithAttribute:NSForegroundColorAttributeName] length]];
+    self.outlinedCharactersLabel.text = [NSString stringWithFormat:@"%d outlined character(s)", [[self charactersWithAttribute:NSStrokeWidthAttributeName] length]];
+}
+
+@end
